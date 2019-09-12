c04390f78ea47ef0a06489accbfaa82670ee465d
limingru <18813070947@163.com>
2019-09-12 19:20:07 +0800

    then方法完成

--- MyPromise.js
+++ MyPromise.js
@@ -1,4 +1,3 @@
-
 function MyPromise(executor) {
   this.status = 'pending'
   this.data = undefined
@@ -23,19 +22,83 @@ function MyPromise(executor) {
   executor(resolve, reject)
 }
 
-MyPromise.prototype.then = function(onResolved, onRejected) {
+MyPromise.prototype.then = function (onResolved, onRejected) {
   let promise2
 
   if (this.status === 'pending') {
-    this.resolvedCallbacks.push(onResolved)
-    this.rejectedCallbacks.push(onRejected)
+    promise2 = new MyPromise((resolve, reject) => {
+      function successFn(value) {
+        let x = onResolved(value)
+        resolve_promise(promise2, x, resolve, reject)
+      }
+
+      function failFn(reason) {
+        let x = onRejected(reason)
+        resolve_promise(promise2, x, resolve, reject)
+      }
+
+      this.resolvedCallbacks.push(successFn)
+      this.rejectedCallbacks.push(failFn)
+    })
   }
 
   if (this.status === 'fulfilled') {
-    onResolved(this.data)
+    promise2 = new MyPromise((resolve, reject) => {
+      setTimeout(() => {
+        let x = onResolved(this.data)
+        resolve_promise(promise2, x, resolve, reject)
+      })
+    })
   }
 
   if (this.status === 'rejected') {
-    onRejected(this.reason)
+    promise2 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let x = onRejected(this.reason)
+        resolve_promise(promise2, x, resolve, reject)
+      })
+    })
+  }
+
+  return promise2
+}
+
+function resolve_promise(promise2, x, resolve, reject) {
+  if (x === promise2) {
+    reject(new TypeError('Chaining cycle detected for promise'))
+    return
+  }
+  if (x instanceof MyPromise) {
+    x.then(function(v) {
+      resolve_promise(promise2, v, resolve, reject)
+    }, function(t) {
+      reject(t)
+    })
+    return
+  }
+  if (x !== null && (typeof x === 'function' || typeof x === 'object')) {
+    let called = false
+    try {
+      let then = x.then
+      if (typeof then === 'function') {
+        then.call(x, function resolvePromise(y) {
+          if (called) return
+          called = true
+          resolve_promise(promise2, y, resolve, reject)
+        }, function rejectPromise(r) {
+          if (called) return
+          called = true
+          reject(r)
+        })
+      } else {
+        resolve(x)
+      }
+    } catch (e) {
+      if (called) return
+      called = true
+      reject(e)
+    }
+  } else {
+    resolve(x)
   }
 }
